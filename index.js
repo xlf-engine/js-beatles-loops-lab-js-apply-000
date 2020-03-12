4465b7cd782f36f6daf65e969a1a66bd9a2ced3a
NICOLAS T <nicolasturek@gmail.com>
2020-03-12 03:21:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // add solution here
 
+//const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+//
+
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (let i = 0; i < 4; i++) {
